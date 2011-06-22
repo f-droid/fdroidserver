c5f3dcb59ad6a805fa27c7552c66add480b7e557
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2011-06-23 02:10:50 +0300

    Support "Category" field in metadata, export as <category> in index.xml.

--- common.py
+++ common.py
@@ -278,6 +278,8 @@ def parse_metadata(metafile, **kw):
                 thisinfo['source'] = value
             elif field == 'License':
                 thisinfo['license'] = value
+            elif field == 'Category':
+                thisinfo['category'] = value
             elif field == 'Web Site':
                 thisinfo['web'] = value
             elif field == 'Issue Tracker':
