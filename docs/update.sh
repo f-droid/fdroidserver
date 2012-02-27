da6f344b8a16bffe663855ab93c3b2267b595ef2
Ciaran Gultnieks <ciaran@ciarang.com>
2012-02-27 20:38:00 +0000

    Docs for importing

--- docs/update.sh
+++ docs/update.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 ./gendocs.sh --email admin@f-droid.org fdroid "F-Droid Server Manual"
 scp -r manual/* fdroid@f-droid.org:public_html/manual/
 rm fdroid.cps fdroid.ky fdroid.vr fdroid.aux fdroid.fn fdroid.log fdroid.toc
