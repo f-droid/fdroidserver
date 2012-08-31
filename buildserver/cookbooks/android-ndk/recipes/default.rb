f63d57b5121c3e02fc1b543336260b386064bbb5
Ciaran Gultnieks <ciaran@ciarang.com>
2012-08-31 20:34:52 +0100

    Correct ndk-r8b install for build server

--- buildserver/cookbooks/android-ndk/recipes/default.rb
+++ buildserver/cookbooks/android-ndk/recipes/default.rb
@@ -8,7 +8,7 @@ script "setup-android-ndk" do
   code "
     wget http://dl.google.com/android/ndk/android-ndk-r8b-linux-x86.tar.bz2
     tar jxvf android-ndk-r8b-linux-x86.tar.bz2
-    mv android-ndk-r8 #{ndk_loc}
+    mv android-ndk-r8b #{ndk_loc}
   "
   not_if do
     File.exists?("#{ndk_loc}")
