8a3ed0ed17c949e8dc23bd36575de1b79e9a56ce
Ciaran Gultnieks <ciaran@ciarang.com>
2012-08-31 16:46:57 +0100

    New ndk for build server

--- buildserver/cookbooks/android-ndk/recipes/default.rb
+++ buildserver/cookbooks/android-ndk/recipes/default.rb
@@ -6,9 +6,9 @@ script "setup-android-ndk" do
   user node[:settings][:user]
   cwd "/tmp"
   code "
-    wget http://dl.google.com/android/ndk/android-ndk-r7-linux-x86.tar.bz2
-    tar jxvf android-ndk-r7-linux-x86.tar.bz2
-    mv android-ndk-r7 #{ndk_loc}
+    wget http://dl.google.com/android/ndk/android-ndk-r8b-linux-x86.tar.bz2
+    tar jxvf android-ndk-r8b-linux-x86.tar.bz2
+    mv android-ndk-r8 #{ndk_loc}
   "
   not_if do
     File.exists?("#{ndk_loc}")
