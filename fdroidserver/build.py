7e9d46d427f49df4f373c004e1f00f72fa72c6d2
Simon Josefsson <simon@josefsson.org>
2013-03-05 13:36:15 +0100

    Discover apk filename from maven builds.

--- fdroidserver/build.py
+++ fdroidserver/build.py
@@ -352,6 +352,10 @@ def build_local(app, thisbuild, vcs, build_dir, output_dir, extlib_dir, tmp_dir,
             m = re.match(r".*^\[INFO\] [^$]*aapt \[package,[^$]*" + app['id'] + "/app/target/([^$]+)\.ap_\]",
                     output, re.S|re.M)
         if not m:
+            # This format is found in com.yubico.yubitotp for example...
+            m = re.match(r".*^\[INFO\] [^$]*aapt \[package,[^$]*" + app['id'] + "/" + thisbuild['bindir'] + "/([^$]+)\.ap_,",
+                    output, re.S|re.M)
+        if not m:
             print output
             raise BuildException('Failed to find output')
         src = m.group(1)
