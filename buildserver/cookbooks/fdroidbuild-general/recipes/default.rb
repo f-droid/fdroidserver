f73857fe714b1ced8c22430c705bef5e80b2b090
Ciaran Gultnieks <ciaran@ciarang.com>
2013-03-13 16:14:48 +0000

    Add autopoint to build server

--- buildserver/cookbooks/fdroidbuild-general/recipes/default.rb
+++ buildserver/cookbooks/fdroidbuild-general/recipes/default.rb
@@ -1,5 +1,5 @@
 
-%w{ant ant-contrib autoconf bison libtool libssl1.0.0 libssl-dev maven javacc python git-core mercurial subversion bzr git-svn make perlmagick pkg-config zip ruby rubygems librmagick-ruby}.each do |pkg|
+%w{ant ant-contrib autoconf autopoint bison libtool libssl1.0.0 libssl-dev maven javacc python git-core mercurial subversion bzr git-svn make perlmagick pkg-config zip ruby rubygems librmagick-ruby}.each do |pkg|
   package pkg do
     action :install
   end
