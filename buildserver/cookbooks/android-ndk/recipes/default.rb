d54031294a09658a81fed53c709c3e03e3f0ce63
Ciaran Gultnieks <ciaran@ciarang.com>
2013-04-02 22:32:49 +0100

    Use ndk r8e

--- buildserver/cookbooks/android-ndk/recipes/default.rb
+++ buildserver/cookbooks/android-ndk/recipes/default.rb
@@ -6,9 +6,9 @@ script "setup-android-ndk" do
   user node[:settings][:user]
   cwd "/tmp"
   code "
-    wget http://dl.google.com/android/ndk/android-ndk-r8b-linux-x86.tar.bz2
-    tar jxvf android-ndk-r8b-linux-x86.tar.bz2
-    mv android-ndk-r8b #{ndk_loc}
+    wget http://dl.google.com/android/ndk/android-ndk-r8e-linux-x86.tar.bz2
+    tar jxvf android-ndk-r8e-linux-x86.tar.bz2
+    mv android-ndk-r8e #{ndk_loc}
   "
   not_if do
     File.exists?("#{ndk_loc}")
