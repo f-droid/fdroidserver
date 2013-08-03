e0174654f847dd32be8e2f386b3246933101b0de
Daniel Martí <mvdan@mvdan.cc>
2013-08-03 17:45:11 +0200

    Try and feed the right gradle apk

--- fdroidserver/build.py
+++ fdroidserver/build.py
@@ -467,6 +467,8 @@ def build_local(app, thisbuild, vcs, build_dir, output_dir, srclib_dir, extlib_d
             raise BuildException('Failed to find output')
         src = m.group(1)
         src = os.path.join(bindir, src) + '.apk'
+    elif 'gradle' in thisbuild:
+        src = os.path.join('build', 'apk', '-'.join(app['Auto Name'], flavour, 'release', 'unsigned')+'.apk')
     else:
         src = re.match(r".*^.*Creating (.+) for release.*$.*", output,
             re.S|re.M).group(1)
