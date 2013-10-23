548c5f606c8770876279c395d5f9b6571443252e
Ciaran Gultnieks <ciaran@ciarang.com>
2013-10-23 15:57:02 +0100

    Removed bogus (hopefully) checkout

--- fdroidserver/common.py
+++ fdroidserver/common.py
@@ -1155,7 +1155,8 @@ def getsrclib(spec, srclib_dir, sdk_path, ndk_path="", mvn3="", basepath=False, 
     if not preponly:
         vcs = getvcs(srclib["Repo Type"], srclib["Repo"], sdir, sdk_path)
         vcs.srclib = (name, sdir)
-        vcs.gotorevision(ref)
+        if ref:
+            vcs.gotorevision(ref)
 
         if raw:
             return vcs
