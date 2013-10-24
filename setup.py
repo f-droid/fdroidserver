4d79f41beaca27604b18a70454802067c48c7166
Hans-Christoph Steiner <hans@eds.org>
2013-10-24 15:29:40 -0400

    convert setup.py into working setuptools-based installer
    With this setup.py, its possible to install the required packages using:
      pip install -e .
    
    The Debian packaging will also automatically get the dependencies from
    install_requires.  This does not handle the generation of the docs at all.
    I have not found a straightforward way to include running ./gendocs.sh in
    setup.py, but its easy to run in the Debian packaging.

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='FDroidServer',
       version='0.1',
       description='F-Droid Server Tools',
+      long_description=open('README').read(),
       author='The F-Droid Project',
       author_email='admin@f-droid.org',
       url='http://f-droid.org',
       packages=['fdroidserver'],
       scripts=['fdroid'],
-      data_files = [('', ['COPYING', 'config.sample.py']),
-                    ('docs', ['docs/*.texi'])
-                   ]
-     )
+      install_requires=[
+        'python-magic',
+        'PIL',
+        ],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX',
+        'Topic :: Utilities',
+        ],
+      )
