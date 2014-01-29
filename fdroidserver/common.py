63a0e859ff81cb03182af68e6c6bfff4d83386d8
Daniel Martí <mvdan@mvdan.cc>
2014-01-29 12:38:21 +0100

    Run submodule reset and clean before update. Also do --recursive for these.

--- fdroidserver/common.py
+++ fdroidserver/common.py
@@ -336,18 +336,18 @@ class vcs_git(vcs):
 
     def initsubmodules(self):
         self.checkrepo()
-        if subprocess.call(['git', 'submodule', 'update',
-            '--init', '--recursive'],
-                cwd=self.local) != 0:
-            raise VCSException("Git submodule update failed")
-        if subprocess.call(['git', 'submodule', 'foreach',
+        if subprocess.call(['git', 'submodule', 'foreach', '--recursive',
             'git', 'reset', '--hard'],
                 cwd=self.local) != 0:
             raise VCSException("Git submodule reset failed")
-        if subprocess.call(['git', 'submodule', 'foreach',
+        if subprocess.call(['git', 'submodule', 'foreach', '--recursive',
             'git', 'clean', '-dffx'],
                 cwd=self.local) != 0:
             raise VCSException("Git submodule clean failed")
+        if subprocess.call(['git', 'submodule', 'update',
+            '--init', '--force', '--recursive'],
+                cwd=self.local) != 0:
+            raise VCSException("Git submodule update failed")
 
     def gettags(self):
         self.checkrepo()
