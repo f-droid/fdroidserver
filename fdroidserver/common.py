a6c1b33a8c711da5cb5ddfe047b14353b40c935e
Ciaran Gultnieks <ciaran@ciarang.com>
2014-02-25 12:46:22 +0000

    Remove unused import

--- fdroidserver/common.py
+++ fdroidserver/common.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import glob, os, sys, re
+import os, sys, re
 import shutil
 import stat
 import subprocess
