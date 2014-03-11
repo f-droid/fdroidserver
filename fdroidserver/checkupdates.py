0b31a7a37c82f16f06b512f3a79a25f7d30ebd9a
Ciaran Gultnieks <ciaran@ciarang.com>
2014-03-11 07:53:30 +0000

    Don't do autoname when update checking is disabled
    Much of that stuff has checking set to None because the source repo is
    not accessible. Attempting to pull it is just filling logs with
    exception messages which are likely to be masking real errors.

--- fdroidserver/checkupdates.py
+++ fdroidserver/checkupdates.py
@@ -398,7 +398,7 @@ def main():
             writeit = True
 
         # Do the Auto Name thing as well as finding the CV real name
-        if len(app["Repo Type"]) > 0:
+        if len(app["Repo Type"]) > 0 and mode != 'None':
 
             try:
 
