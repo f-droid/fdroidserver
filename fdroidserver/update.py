95d8f294797cd81cc7a3a2be1a769dbf1a936b1e
Ciaran Gultnieks <ciaran@ciarang.com>
2014-06-25 11:43:23 +0100

    Fix nasty problem caused by 7c2e61a4

--- fdroidserver/update.py
+++ fdroidserver/update.py
@@ -664,7 +664,7 @@ def make_index(apps, apks, repodir, archive, categories):
                              '-alias', config['repo_keyalias'],
                              '-keystore', config['keystore'],
                              '-storepass:file', config['keystorepassfile']]
-                            + config['smartcardoptions'])
+                            + config['smartcardoptions'], output=False)
             if p.returncode != 0:
                 msg = "Failed to get repo pubkey!"
                 if config['keystore'] == 'NONE':
