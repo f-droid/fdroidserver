e26ad377a62065f243126e78808790c1888049bc
Ciaran Gultnieks <ciaran@ciarang.com>
2015-01-27 07:13:21 +0000

    Handle not-found package name gracefully

--- fdroidserver/common.py
+++ fdroidserver/common.py
@@ -1055,7 +1055,7 @@ def parse_androidmanifests(paths, ignoreversions=None):
     if max_version is None:
         max_version = "Unknown"
 
-    if not is_valid_package_name(max_package):
+    if max_package and not is_valid_package_name(max_package):
         raise FDroidException("Invalid package name {0}".format(max_package))
 
     return (max_version, max_vercode, max_package)
