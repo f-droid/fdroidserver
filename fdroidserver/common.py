f-droid/fdroidserver

commit 021df3c42420de542bf11f22948831023b47ebf3

Other minor file reading fixes

fdroidserver/common.py
@@ -945,16 +945,15 @@ def get_library_references(root_dir):
     proppath = os.path.join(root_dir, 'project.properties')
     if not os.path.isfile(proppath):
         return libraries
-    with open(proppath) as f:
-        for line in f.readlines():
-            if not line.startswith('android.library.reference.'):
-                continue
-            path = line.split('=')[1].strip()
-            relpath = os.path.join(root_dir, path)
-            if not os.path.isdir(relpath):
-                continue
-            logging.debug("Found subproject at %s" % path)
-            libraries.append(path)
+    for line in file(proppath):
+        if not line.startswith('android.library.reference.'):
+            continue
+        path = line.split('=')[1].strip()
+        relpath = os.path.join(root_dir, path)
+        if not os.path.isdir(relpath):
+            continue
+        logging.debug("Found subproject at %s" % path)
+        libraries.append(path)
     return libraries
 
 
@@ -1884,9 +1883,11 @@ def place_srclib(root_dir, number, libpath):
     proppath = os.path.join(root_dir, 'project.properties')
 
     lines = []
-    if os.path.isfile(proppath):
-        with open(proppath, "r") as o:
-            lines = o.readlines()
+    if not os.path.isfile(proppath):
+        return
+
+    with open(proppath, "r") as o:
+        lines = o.readlines()
 
     with open(proppath, "w") as o:
         placed = False

commit 96e4062bcf2446e635e522e8b71ced178140edde

Fix project.properties dependency issue
Correctly place scrlib references also
in the case there is no project.properties file
present in the original repository.

This commit fixes a problem, which was introduced with
021df3c42420de542bf11f22948831023b47ebf3: If no
project.properties file exists in the original
project's source repo, place_srclib will not place
a dependency --- even if there should be one:

>    if not os.path.isfile(proppath):
>        return

Example app: fdroiddata/metadata/org.berlin_vegan.bvapp.txt

fdroidserver/common.py
@@ -1886,11 +1886,9 @@ def place_srclib(root_dir, number, libpath):
     proppath = os.path.join(root_dir, 'project.properties')
 
     lines = []
-    if not os.path.isfile(proppath):
-        return
-
-    with open(proppath, "r") as o:
-        lines = o.readlines()
+    if os.path.isfile(proppath):
+        with open(proppath, "r") as o:
+            lines = o.readlines()
 
     with open(proppath, "w") as o:
         placed = False