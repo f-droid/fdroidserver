128b19e0108178a6d5af2b8ddc1509112b4d4fb3
Daniel Martí <mvdan@mvdan.cc>
2015-06-03 21:00:15 +0200

    Enforce https uniformly in all http links

--- fdroidserver/lint.py
+++ fdroidserver/lint.py
@@ -35,6 +35,12 @@ def enforce_https(domain):
 http_warnings = [
     (re.compile(r'.*/$'),
      "HTTP links shouldn't end with a slash"),
+    enforce_https('github.com'),
+    enforce_https('gitorious.org'),
+    enforce_https('apache.org'),
+    enforce_https('google.com'),
+    enforce_https('svn.code.sf.net'),
+    enforce_https('googlecode.com'),
     # TODO enable in August 2015, when Google Code goes read-only
     # (re.compile(r'.*://code\.google\.com/.*'),
     #  "code.google.com will be soon switching down, perhaps the project moved to github.com?"),
@@ -46,12 +52,6 @@ regex_warnings = {
     'Source Code': http_warnings + [
     ],
     'Repo': [
-        enforce_https('github.com'),
-        enforce_https('gitorious.org'),
-        enforce_https('apache.org'),
-        enforce_https('google.com'),
-        enforce_https('svn.code.sf.net'),
-        enforce_https('googlecode.com'),
     ],
     'Issue Tracker': http_warnings + [
         (re.compile(r'.*github\.com/[^/]+/[^/]+[/]*$'),
