fba632cf07fba9f8e7d79993bbd6af1a313ec457
Ciaran Gultnieks <ciaran@ciarang.com>
2015-07-21 22:12:52 +0200

    Update recommended base box

--- examples/makebs.config.py
+++ examples/makebs.config.py
@@ -3,13 +3,13 @@
 # You may want to alter these before running ./makebuildserver
 
 # Name of the base box to use
-basebox = "testing32"
+basebox = "jessie32"
 
 # Location where testing32.box can be found, if you don't already have
 # it. For security reasons, it's recommended that you make your own
 # in a secure environment using trusted media (see the manual) but
 # you can use this default if you like...
-baseboxurl = "https://f-droid.org/testing32.box"
+baseboxurl = "https://f-droid.org/jessie32.box"
 
 # The amount of RAM the build server will have
 memory = 3584
