bace7e912f9efec67074d02c486b0421517ca166
Daniel Martí <mvdan@mvdan.cc>
2015-08-28 15:53:59 -0700

    Add missing -v and -q to readmeta

--- fdroidserver/readmeta.py
+++ fdroidserver/readmeta.py
@@ -25,6 +25,10 @@ import metadata
 def main():
 
     parser = OptionParser(usage="Usage: %prog")
+    parser.add_option("-v", "--verbose", action="store_true", default=False,
+                      help="Spew out even more information than normal")
+    parser.add_option("-q", "--quiet", action="store_true", default=False,
+                      help="Restrict output to warnings and errors")
     parser.parse_args()
     common.read_config(None)
 
