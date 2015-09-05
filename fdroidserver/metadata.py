f-droid/fdroidserver

commit 2831b3e93f5cdf0f91d87936b721976da327529f

convert internal representation of AntiFeatures to list
The AntiFeatures metadata is a comma-separated list of tags, like
Categories, so it should also be stored internally as a list.  This makes
parsing XML and JSON easier.

The test cases' .pickle files look like they change a lot, but they really
don't, its only the change of default AntiFeatures value from None to []

fdroidserver/metadata.py
@@ -42,7 +42,7 @@ class MetaDataException(Exception):
 # In the order in which they are laid out on files
 app_defaults = OrderedDict([
     ('Disabled', None),
-    ('AntiFeatures', None),
+    ('AntiFeatures', []),
     ('Provides', None),
     ('Categories', ['None']),
     ('License', 'Unknown'),
@@ -516,7 +516,7 @@ def read_metadata(xref=True):
 def metafieldtype(name):
     if name in ['Description', 'Maintainer Notes']:
         return 'multiline'
-    if name in ['Categories']:
+    if name in ['Categories', 'AntiFeatures']:
         return 'list'
     if name == 'Build Version':
         return 'build'
@@ -929,7 +929,7 @@ def write_metadata(dest, app):
 
     mf = open(dest, 'w')
     writefield_nonempty('Disabled')
-    writefield_nonempty('AntiFeatures')
+    writefield('AntiFeatures')
     writefield_nonempty('Provides')
     writefield('Categories')
     writefield('License')

fdroidserver/stats.py
@@ -256,8 +256,7 @@ def main():
     for app in metaapps:
         if app['AntiFeatures'] is None:
             continue
-        antifeatures = [a.strip() for a in app['AntiFeatures'].split(',')]
-        for antifeature in antifeatures:
+        for antifeature in app['AntiFeatures']:
             afs[antifeature] += 1
     with open('stats/antifeatures.txt', 'w') as f:
         for antifeature, count in afs.most_common():

fdroidserver/update.py
@@ -92,8 +92,8 @@ def update_wiki(apps, sortedids, apks):
         wikidata = ''
         if app['Disabled']:
             wikidata += '{{Disabled|' + app['Disabled'] + '}}\n'
-        if app['AntiFeatures']:
-            for af in app['AntiFeatures'].split(','):
+        if 'AntiFeatures' in app:
+            for af in app['AntiFeatures']:
                 wikidata += '{{AntiFeature|' + af + '}}\n'
         wikidata += '{{App|id=%s|name=%s|added=%s|lastupdated=%s|source=%s|tracker=%s|web=%s|changelog=%s|donate=%s|flattr=%s|bitcoin=%s|litecoin=%s|dogecoin=%s|license=%s|root=%s}}\n' % (
             appid,
@@ -847,7 +847,7 @@ def make_index(apps, sortedids, apks, repodir, archive, categories):
         addElement('marketvercode', app['Current Version Code'], doc, apel)
 
         if app['AntiFeatures']:
-            af = app['AntiFeatures'].split(',')
+            af = app['AntiFeatures']
             if af:
                 addElementNonEmpty('antifeatures', ','.join(af), doc, apel)
         if app['Provides']:

tests/metadata/org.smssecure.smssecure.pickle
@@ -5,751 +5,752 @@ NsS'Bitcoin'
 p2
 NsS'AntiFeatures'
 p3
-NsS'Web Site'
-p4
-S'http://www.smssecure.org'
+(lp4
+sS'Web Site'
 p5
-sS'Auto Update Mode'
+S'http://www.smssecure.org'
 p6
-S'Version v%v'
+sS'Auto Update Mode'
 p7
-sS'Provides'
+S'Version v%v'
 p8
-NsS'Issue Tracker'
+sS'Provides'
 p9
-S'https://github.com/SMSSecure/SMSSecure/issues'
+NsS'Issue Tracker'
 p10
-sS'Donate'
+S'https://github.com/SMSSecure/SMSSecure/issues'
 p11
-NsS'Repo Type'
+sS'Donate'
 p12
-S'git'
+NsS'Repo Type'
 p13
-sS'Description'
+S'git'
 p14
-(lp15
+sS'Description'
+p15
+(lp16
 S'SMSSecure is an SMS/MMS application that allows you to protect your privacy while communicating with friends.'
-p16
-aS'Using SMSSecure, you can send SMS messages and share media or attachments with complete privacy.'
 p17
-aS''
+aS'Using SMSSecure, you can send SMS messages and share media or attachments with complete privacy.'
 p18
-aS"* Easy. SMSSecure works like any other SMS application. There's nothing to sign up for and no new service your friends need to join."
+aS''
 p19
-aS'* Reliable. SMSSecure communicates using encrypted SMS messages. No servers or internet connection required.'
+aS"* Easy. SMSSecure works like any other SMS application. There's nothing to sign up for and no new service your friends need to join."
 p20
-aS'* Private. SMSSecure uses the TextSecure encryption protocol to provide privacy for every message, every time.'
+aS'* Reliable. SMSSecure communicates using encrypted SMS messages. No servers or internet connection required.'
 p21
-aS'* Safe. All messages are encrypted locally, so if your phone is lost or stolen, your messages are protected.'
+aS'* Private. SMSSecure uses the TextSecure encryption protocol to provide privacy for every message, every time.'
 p22
-aS'* Open Source. SMSSecure is Free and Open Source, enabling anyone to verify its security by auditing the code.'
+aS'* Safe. All messages are encrypted locally, so if your phone is lost or stolen, your messages are protected.'
 p23
-asS'Requires Root'
+aS'* Open Source. SMSSecure is Free and Open Source, enabling anyone to verify its security by auditing the code.'
 p24
+asS'Requires Root'
+p25
 I00
 sS'comments'
-p25
-(lp26
+p26
+(lp27
 sS'id'
-p27
-S'org.smssecure.smssecure'
 p28
-sS'Repo'
+S'org.smssecure.smssecure'
 p29
-S'https://github.com/SMSSecure/SMSSecure'
+sS'Repo'
 p30
-sS'No Source Since'
-p31
-g18
-sS'Auto Name'
-p32
-S'SMSSecure'
-p33
-sS'Categories'
-p34
-(lp35
-S'Phone & SMS'
-p36
-asS'Source Code'
-p37
 S'https://github.com/SMSSecure/SMSSecure'
+p31
+sS'No Source Since'
+p32
+g19
+sS'Auto Name'
+p33
+S'SMSSecure'
+p34
+sS'Categories'
+p35
+(lp36
+S'Phone & SMS'
+p37
+asS'Source Code'
 p38
-sS'Litecoin'
+S'https://github.com/SMSSecure/SMSSecure'
 p39
-NsS'Update Check Ignore'
+sS'Litecoin'
 p40
-NsS'Name'
+NsS'Update Check Ignore'
 p41
-NsS'License'
+NsS'Name'
 p42
-S'GPLv3'
+NsS'License'
 p43
-sS'Changelog'
+S'GPLv3'
 p44
-g18
-sS'Update Check Mode'
+sS'Changelog'
 p45
-S'Tags'
+g19
+sS'Update Check Mode'
 p46
-sS'Summary'
+S'Tags'
 p47
-S'Send encrypted text messages (SMS)'
+sS'Summary'
 p48
-sS'Dogecoin'
+S'Send encrypted text messages (SMS)'
 p49
-NsS'Maintainer Notes'
+sS'Dogecoin'
 p50
-(lp51
+NsS'Maintainer Notes'
+p51
+(lp52
 sS'Current Version Code'
-p52
-S'102'
 p53
-sS'Binaries'
+S'102'
 p54
-NsS'Archive Policy'
+sS'Binaries'
 p55
-NsS'builds'
+NsS'Archive Policy'
 p56
-(lp57
-(dp58
+NsS'builds'
+p57
+(lp58
+(dp59
 S'submodules'
-p59
+p60
 I00
 sS'vercode'
-p60
-S'5'
 p61
-sS'forceversion'
+S'5'
 p62
-I00
-sS'oldsdkloc'
+sS'forceversion'
 p63
 I00
-sS'kivy'
+sS'oldsdkloc'
 p64
 I00
-sS'patch'
+sS'kivy'
 p65
-(lp66
+I00
+sS'patch'
+p66
+(lp67
 sS'scanignore'
-p67
-(lp68
+p68
+(lp69
 sS'srclibs'
-p69
-(lp70
+p70
+(lp71
 S'GradleWitness@10f1269c0aafdc1d478efc005ed48f3a47d44278'
-p71
-aS'PreferenceFragment@717a45433b927d2f0dfc5328f79e77c9682c37bc'
 p72
-aS'ShortcutBadger@3815ce2ec0c66acd7d7c0b4f2479df8fa70fed87'
+aS'PreferenceFragment@717a45433b927d2f0dfc5328f79e77c9682c37bc'
 p73
-aS'AospMms@android-5.1.0_r3'
+aS'ShortcutBadger@3815ce2ec0c66acd7d7c0b4f2479df8fa70fed87'
 p74
-asS'encoding'
+aS'AospMms@android-5.1.0_r3'
 p75
-NsS'extlibs'
+asS'encoding'
 p76
-(lp77
+NsS'extlibs'
+p77
+(lp78
 sS'init'
-p78
-g18
-sS'version'
 p79
-S'0.3.3'
+g19
+sS'version'
 p80
-sS'subdir'
+S'0.3.3'
 p81
-NsS'rm'
+sS'subdir'
 p82
-(lp83
+NsS'rm'
+p83
+(lp84
 S'libs/*'
-p84
-asS'type'
 p85
-S'gradle'
+asS'type'
 p86
-sS'build'
+S'gradle'
 p87
-g18
-sS'ndk_path'
+sS'build'
 p88
-g18
-sS'forcevercode'
+g19
+sS'ndk_path'
 p89
+g19
+sS'forcevercode'
+p90
 I01
 sS'preassemble'
-p90
-(lp91
+p91
+(lp92
 sS'update'
-p92
-(lp93
+p93
+(lp94
 S'auto'
-p94
-asS'maven'
 p95
+asS'maven'
+p96
 I00
 sS'disable'
-p96
-S'builds, merge changes into upstream'
 p97
-sS'commit'
+S'builds, merge changes into upstream'
 p98
-S'66367479a4f57f347b5cbe8f6f8f632adaae7727'
+sS'commit'
 p99
-sS'scandelete'
+S'66367479a4f57f347b5cbe8f6f8f632adaae7727'
 p100
-(lp101
+sS'scandelete'
+p101
+(lp102
 sS'buildjni'
-p102
-(lp103
+p103
+(lp104
 sS'ndk'
-p104
-S'r10e'
 p105
-sS'target'
+S'r10e'
 p106
-NsS'antcommands'
+sS'target'
 p107
-NsS'gradle'
+NsS'antcommands'
 p108
-(lp109
+NsS'gradle'
+p109
+(lp110
 S'yes'
-p110
-asS'prebuild'
 p111
-S"touch signing.properties && pushd $$GradleWitness$$ && gradle jar && popd && cp $$GradleWitness$$/build/libs/GradleWitness.jar libs/gradle-witness.jar && sed -i -e '20,22d' build.gradle && pushd $$PreferenceFragment$$ && gradle uploadArchives && popd && sed -i -e '/5470f5872514a6226fa1fc6f4e000991f38805691c534cf0bd2778911fc773ad/d' build.gradle && mkdir smil && pushd smil && wget -c http://www.w3.org/TR/smil-boston-dom/java-binding.zip && unzip java-binding.zip && popd && cp -fR smil/java/org src/ && rm -fR smil && sed -i -e '/org.w3c.smil/d' build.gradle && cp -fR $$AospMms$$/src/org src/ "
+asS'prebuild'
 p112
-sS'novcheck'
+S"touch signing.properties && pushd $$GradleWitness$$ && gradle jar && popd && cp $$GradleWitness$$/build/libs/GradleWitness.jar libs/gradle-witness.jar && sed -i -e '20,22d' build.gradle && pushd $$PreferenceFragment$$ && gradle uploadArchives && popd && sed -i -e '/5470f5872514a6226fa1fc6f4e000991f38805691c534cf0bd2778911fc773ad/d' build.gradle && mkdir smil && pushd smil && wget -c http://www.w3.org/TR/smil-boston-dom/java-binding.zip && unzip java-binding.zip && popd && cp -fR smil/java/org src/ && rm -fR smil && sed -i -e '/org.w3c.smil/d' build.gradle && cp -fR $$AospMms$$/src/org src/ "
 p113
+sS'novcheck'
+p114
 I00
 sS'output'
-p114
-Nsa(dp115
+p115
+Nsa(dp116
 S'submodules'
-p116
-I01
-sg60
-S'6'
 p117
-sg62
-I00
-sg63
-I00
-sg64
-I00
-sg65
-g66
-sg67
-g68
-sS'srclibs'
+I01
+sg61
+S'6'
 p118
-(lp119
-S'GradleWitness@10f1269c0aafdc1d478efc005ed48f3a47d44278'
-p120
-asg75
-Nsg76
-g77
-sg78
-g18
-sg79
-S'0.3.3'
-p121
-sg81
-NsS'rm'
-p122
-(lp123
-S'libs/*.jar'
-p124
-asg85
-g86
-sg87
-g18
-sg88
-g18
-sS'forcevercode'
-p125
-I00
-sg90
-g91
-sg92
-g93
-sg95
-I00
-sS'disable'
-p126
-S'builds, wait for upstream'
-p127
-sS'commit'
-p128
-S'9675ce5eecb929dcaddb43b3d9486fdb88b9ae1a'
-p129
-sg100
-g101
-sg102
-g103
-sg104
-g105
-sg106
-Nsg107
-NsS'gradle'
-p130
-(lp131
-g110
-asS'prebuild'
-p132
-S'touch signing.properties && pushd $$GradleWitness$$ && gradle jar && popd && cp $$GradleWitness$$/build/libs/GradleWitness.jar libs/gradle-witness.jar'
-p133
-sg113
-I00
-sg114
-Nsa(dp134
-S'submodules'
-p135
-I01
-sg60
-S'9'
-p136
-sg62
-I00
 sg63
 I00
 sg64
 I00
 sg65
-g66
-sg67
-g68
+I00
+sg66
+g67
+sg68
+g69
 sS'srclibs'
+p119
+(lp120
+S'GradleWitness@10f1269c0aafdc1d478efc005ed48f3a47d44278'
+p121
+asg76
+Nsg77
+g78
+sg79
+g19
+sg80
+S'0.3.3'
+p122
+sg82
+NsS'rm'
+p123
+(lp124
+S'libs/*.jar'
+p125
+asg86
+g87
+sg88
+g19
+sg89
+g19
+sS'forcevercode'
+p126
+I00
+sg91
+g92
+sg93
+g94
+sg96
+I00
+sS'disable'
+p127
+S'builds, wait for upstream'
+p128
+sS'commit'
+p129
+S'9675ce5eecb929dcaddb43b3d9486fdb88b9ae1a'
+p130
+sg101
+g102
+sg103
+g104
+sg105
+g106
+sg107
+Nsg108
+NsS'gradle'
+p131
+(lp132
+g111
+asS'prebuild'
+p133
+S'touch signing.properties && pushd $$GradleWitness$$ && gradle jar && popd && cp $$GradleWitness$$/build/libs/GradleWitness.jar libs/gradle-witness.jar'
+p134
+sg114
+I00
+sg115
+Nsa(dp135
+S'submodules'
+p136
+I01
+sg61
+S'9'
 p137
-(lp138
-sg75
-Nsg76
-g77
-sg78
-g18
+sg63
+I00
+sg64
+I00
+sg65
+I00
+sg66
+g67
+sg68
+g69
+sS'srclibs'
+p138
+(lp139
+sg76
+Nsg77
+g78
 sg79
+g19
+sg80
 S'0.4.2'
-p139
-sg81
-NsS'rm'
 p140
-(lp141
+sg82
+NsS'rm'
+p141
+(lp142
 S'libs/*.jar'
-p142
-asg85
-g86
-sg87
-g18
-sg88
-g18
-sg125
-I00
-sg90
-g91
-sg92
-g93
-sg95
-I00
-sS'disable'
 p143
-S'builds locally, but not on BS'
+asg86
+g87
+sg88
+g19
+sg89
+g19
+sg126
+I00
+sg91
+g92
+sg93
+g94
+sg96
+I00
+sS'disable'
 p144
-sS'commit'
+S'builds locally, but not on BS'
 p145
-S'v0.4.2'
+sS'commit'
 p146
-sg100
-g101
-sg102
-g103
-sg104
-g105
-sg106
-Nsg107
-NsS'gradle'
+S'v0.4.2'
 p147
-(lp148
-g110
+sg101
+g102
+sg103
+g104
+sg105
+g106
+sg107
+Nsg108
+NsS'gradle'
+p148
+(lp149
+g111
 asS'prebuild'
-p149
-S'touch signing.properties && ./build-witness.sh && rm -rf libs/gradle-witness/build && echo "org.gradle.jvmargs=-Xms512m -Xmx512m -XX:MaxPermSize=512m" >> gradle.properties'
 p150
-sg113
-I00
-sg114
-Nsa(dp151
-S'submodules'
-p152
-I01
-sg60
-S'11'
-p153
-sg62
-I00
-sg63
-I00
-sg64
-I00
-sg65
-g66
-sg67
-g68
-sg137
-g138
-sg75
-Nsg76
-g77
-sg78
-g18
-sg79
-S'0.5.1'
-p154
-sg81
-NsS'rm'
-p155
-(lp156
-S'libs/*.jar'
-p157
-asg85
-g86
-sg87
-g18
-sg88
-g18
-sg125
-I00
-sg90
-g91
-sg92
-g93
-sg95
-I00
-sS'disable'
-p158
-I00
-sS'commit'
-p159
-S'v0.5.1'
-p160
-sg100
-g101
-sg102
-g103
-sg104
-g105
-sg106
-Nsg107
-NsS'gradle'
-p161
-(lp162
-g110
-asS'prebuild'
-p163
 S'touch signing.properties && ./build-witness.sh && rm -rf libs/gradle-witness/build && echo "org.gradle.jvmargs=-Xms512m -Xmx512m -XX:MaxPermSize=512m" >> gradle.properties'
-p164
-sg113
-I00
+p151
 sg114
-Nsa(dp165
-S'submodules'
-p166
-I01
-sg60
-S'12'
-p167
-sg62
 I00
+sg115
+Nsa(dp152
+S'submodules'
+p153
+I01
+sg61
+S'11'
+p154
 sg63
 I00
 sg64
 I00
 sg65
-g66
-sg67
-g68
-sg137
-g138
-sg75
-Nsg76
-g77
-sg78
-g18
-sg79
-S'0.5.2'
-p168
-sg81
-NsS'rm'
-p169
-(lp170
-S'libs/*.jar'
-p171
-asg85
-g86
-sg87
-g18
-sg88
-g18
-sg125
 I00
-sg90
-g91
-sg92
-g93
-sg95
+sg66
+g67
+sg68
+g69
+sg138
+g139
+sg76
+Nsg77
+g78
+sg79
+g19
+sg80
+S'0.5.1'
+p155
+sg82
+NsS'rm'
+p156
+(lp157
+S'libs/*.jar'
+p158
+asg86
+g87
+sg88
+g19
+sg89
+g19
+sg126
+I00
+sg91
+g92
+sg93
+g94
+sg96
 I00
 sS'disable'
+p159
+I00
+sS'commit'
+p160
+S'v0.5.1'
+p161
+sg101
+g102
+sg103
+g104
+sg105
+g106
+sg107
+Nsg108
+NsS'gradle'
+p162
+(lp163
+g111
+asS'prebuild'
+p164
+S'touch signing.properties && ./build-witness.sh && rm -rf libs/gradle-witness/build && echo "org.gradle.jvmargs=-Xms512m -Xmx512m -XX:MaxPermSize=512m" >> gradle.properties'
+p165
+sg114
+I00
+sg115
+Nsa(dp166
+S'submodules'
+p167
+I01
+sg61
+S'12'
+p168
+sg63
+I00
+sg64
+I00
+sg65
+I00
+sg66
+g67
+sg68
+g69
+sg138
+g139
+sg76
+Nsg77
+g78
+sg79
+g19
+sg80
+S'0.5.2'
+p169
+sg82
+NsS'rm'
+p170
+(lp171
+S'libs/*.jar'
 p172
-S'broken in upstream'
+asg86
+g87
+sg88
+g19
+sg89
+g19
+sg126
+I00
+sg91
+g92
+sg93
+g94
+sg96
+I00
+sS'disable'
 p173
-sS'commit'
+S'broken in upstream'
 p174
-S'v0.5.2'
+sS'commit'
 p175
-sg100
-g101
-sg102
-g103
-sg104
-g105
-sg106
-Nsg107
-NsS'gradle'
+S'v0.5.2'
 p176
-(lp177
-g110
+sg101
+g102
+sg103
+g104
+sg105
+g106
+sg107
+Nsg108
+NsS'gradle'
+p177
+(lp178
+g111
 asS'prebuild'
-p178
-S'touch signing.properties && ./scripts/build-witness.sh && rm -rf libs/gradle-witness/build'
 p179
-sg113
-I00
+S'touch signing.properties && ./scripts/build-witness.sh && rm -rf libs/gradle-witness/build'
+p180
 sg114
-Nsa(dp180
+I00
+sg115
+Nsa(dp181
 S'submodules'
-p181
-I01
-sg60
-S'100'
 p182
-sg62
-I00
-sg63
-I00
-sg64
-I00
-sg65
-g66
-sg67
-g68
-sg137
-g138
-sg75
-Nsg76
-g77
-sg78
-g18
-sg79
-S'0.5.3'
+I01
+sg61
+S'100'
 p183
-sg81
-NsS'rm'
+sg63
+I00
+sg64
+I00
+sg65
+I00
+sg66
+g67
+sg68
+g69
+sg138
+g139
+sg76
+Nsg77
+g78
+sg79
+g19
+sg80
+S'0.5.3'
 p184
-(lp185
+sg82
+NsS'rm'
+p185
+(lp186
 S'libs/*.jar'
-p186
-asg85
-g86
-sg87
-g18
-sg88
-g18
-sg125
-I00
-sg90
-g91
-sg92
-g93
-sg95
-I00
-sg158
-I00
-sS'commit'
 p187
-S'v0.5.3'
+asg86
+g87
+sg88
+g19
+sg89
+g19
+sg126
+I00
+sg91
+g92
+sg93
+g94
+sg96
+I00
+sg159
+I00
+sS'commit'
 p188
-sg100
-g101
-sg102
-g103
-sg104
-g105
-sg106
-Nsg107
-NsS'gradle'
+S'v0.5.3'
 p189
-(lp190
-g110
+sg101
+g102
+sg103
+g104
+sg105
+g106
+sg107
+Nsg108
+NsS'gradle'
+p190
+(lp191
+g111
 asS'prebuild'
-p191
-S'touch signing.properties && ./scripts/build-witness.sh && rm -rf libs/gradle-witness/build'
 p192
-sg113
-I00
+S'touch signing.properties && ./scripts/build-witness.sh && rm -rf libs/gradle-witness/build'
+p193
 sg114
-Nsa(dp193
+I00
+sg115
+Nsa(dp194
 S'submodules'
-p194
-I01
-sg60
-S'101'
 p195
-sg62
-I00
-sg63
-I00
-sg64
-I00
-sg65
-g66
-sg67
-g68
-sg137
-g138
-sg75
-Nsg76
-g77
-sg78
-g18
-sg79
-S'0.5.4'
-p196
-sg81
-NsS'rm'
-p197
-(lp198
-S'libs/*.jar'
-p199
-asg85
-g86
-sg87
-g18
-sg88
-g18
-sg125
-I00
-sg90
-g91
-sg92
-g93
-sg95
-I00
-sg158
-I00
-sS'commit'
-p200
-S'v0.5.4'
-p201
-sg100
-g101
-sg102
-g103
-sg104
-g105
-sg106
-Nsg107
-NsS'gradle'
-p202
-(lp203
-g110
-asS'prebuild'
-p204
-S'touch signing.properties && ./scripts/build-witness.sh && rm -rf libs/gradle-witness/build'
-p205
-sg113
-I00
-sg114
-Nsa(dp206
-S'submodules'
-p207
 I01
-sg60
-S'102'
-p208
-sg62
-I00
+sg61
+S'101'
+p196
 sg63
 I00
 sg64
 I00
 sg65
-g66
-sg67
-g68
-sg137
-g138
-sg75
-Nsg76
-g77
-sg78
-g18
+I00
+sg66
+g67
+sg68
+g69
+sg138
+g139
+sg76
+Nsg77
+g78
 sg79
-S'0.6.0'
-p209
-sg81
+g19
+sg80
+S'0.5.4'
+p197
+sg82
 NsS'rm'
-p210
-(lp211
+p198
+(lp199
 S'libs/*.jar'
-p212
-asg85
-g86
-sg87
-g18
+p200
+asg86
+g87
 sg88
-g18
-sg125
+g19
+sg89
+g19
+sg126
 I00
-sg90
-g91
-sg92
-g93
-sg95
+sg91
+g92
+sg93
+g94
+sg96
 I00
-sg158
+sg159
 I00
 sS'commit'
-p213
-S'v0.6.0'
-p214
-sg100
-g101
-sg102
-g103
-sg104
-g105
-sg106
-Nsg107
+p201
+S'v0.5.4'
+p202
+sg101
+g102
+sg103
+g104
+sg105
+g106
+sg107
+Nsg108
 NsS'gradle'
-p215
-(lp216
-g110
+p203
+(lp204
+g111
 asS'prebuild'
-p217
+p205
 S'touch signing.properties && ./scripts/build-witness.sh && rm -rf libs/gradle-witness/build'
-p218
-sg113
-I00
+p206
 sg114
-NsasS'FlattrID'
-p219
-NsS'Disabled'
-p220
-NsS'Update Check Name'
-p221
-NsS'Vercode Operation'
-p222
-NsS'Current Version'
-p223
+I00
+sg115
+Nsa(dp207
+S'submodules'
+p208
+I01
+sg61
+S'102'
+p209
+sg63
+I00
+sg64
+I00
+sg65
+I00
+sg66
+g67
+sg68
+g69
+sg138
+g139
+sg76
+Nsg77
+g78
+sg79
+g19
+sg80
 S'0.6.0'
+p210
+sg82
+NsS'rm'
+p211
+(lp212
+S'libs/*.jar'
+p213
+asg86
+g87
+sg88
+g19
+sg89
+g19
+sg126
+I00
+sg91
+g92
+sg93
+g94
+sg96
+I00
+sg159
+I00
+sS'commit'
+p214
+S'v0.6.0'
+p215
+sg101
+g102
+sg103
+g104
+sg105
+g106
+sg107
+Nsg108
+NsS'gradle'
+p216
+(lp217
+g111
+asS'prebuild'
+p218
+S'touch signing.properties && ./scripts/build-witness.sh && rm -rf libs/gradle-witness/build'
+p219
+sg114
+I00
+sg115
+NsasS'FlattrID'
+p220
+NsS'Disabled'
+p221
+NsS'Update Check Name'
+p222
+NsS'Vercode Operation'
+p223
+NsS'Current Version'
 p224
+S'0.6.0'
+p225
 s.

commit de1c5991272b251a27a6d92cbd790497676ad275

Fix regression introduced in 2831b3e93
An empty list of antifeatures should not be written.

fdroidserver/metadata.py
@@ -1047,7 +1047,8 @@ def write_metadata(dest, app):
 
     mf = open(dest, 'w')
     writefield_nonempty('Disabled')
-    writefield('AntiFeatures')
+    if app['AntiFeatures']:
+        writefield('AntiFeatures')
     writefield_nonempty('Provides')
     writefield('Categories')
     writefield('License')