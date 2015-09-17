1683df0cf8c0b75267a2cb3761a06f095551f4d0
NeroBurner <pyro4hell@gmail.com>
2015-09-17 13:33:19 +0200

    handle Queue

--- fdroidserver/common.py
+++ fdroidserver/common.py
@@ -29,11 +29,17 @@ import stat
 import subprocess
 import time
 import operator
-import Queue
 import logging
 import hashlib
 import socket
 import xml.etree.ElementTree as XMLElementTree
+
+try:
+    # Python 2
+    from Queue import Queue
+except ImportError:
+    # Python 3
+    from queue import Queue
 
 from zipfile import ZipFile
 
@@ -1606,7 +1612,7 @@ def FDroidPopen(commands, cwd=None, output=True):
         raise BuildException("OSError while trying to execute " +
                              ' '.join(commands) + ': ' + str(e))
 
-    stdout_queue = Queue.Queue()
+    stdout_queue = Queue()
     stdout_reader = AsynchronousFileReader(p.stdout, stdout_queue)
 
     # Check the queue for output (until there is no more to get)
