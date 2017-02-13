8decd3b504378eb962d48e140d0edf2e598df22b
Hans-Christoph Steiner <hans@eds.org>
2017-02-13 22:36:28 +0100

    remove TeX manual, it moved to https://gitlab.com/fdroid/fdroid-website
    The whole TeX manual has been folded into the new Jekyll-based website, so
    this is no longer needed at all.

--- docs/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-./gendocs.sh --email admin@f-droid.org fdroid "F-Droid Server Manual"
-scp -r manual/* fdroid@f-droid.org:public_html/manual/
-rm fdroid.cps fdroid.ky fdroid.vr fdroid.aux fdroid.fn fdroid.log fdroid.toc
-rm fdroid.cp  fdroid.info fdroid.pg fdroid.tp
-
