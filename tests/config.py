56ef716a4eba81fe5b7365443e8e26adf8c2420e
Hans-Christoph Steiner <hans@eds.org>
2017-02-23 21:23:45 +0100

    test for original "v0" index XML compatibility
    The original index.xml format needs to stay around for backwards
    compatibility, but we shouldn't touch it anymore once the new format is in
    place.  This is a test to make sure `fdroid update` can still generate the
    correct XML.
    
    install_list and uninstall_list should be tuples or lists in order to
    ensure that the order is preserved.
    
    These tests also check that the added and lastupdated dates are
    working correct, based on the dates in tests/stats/known_apks.txt. I
    could see no useful way to test the timestamp, it is just hardcoded
    using a regexp search-and-replace.  Running these tests manually might
    require deleting tmp/apkcache.

---
+++ tests/config.py
@@ -0,0 +1,35 @@
+
+repo_url = "https://MyFirstFDroidRepo.org/fdroid/repo"
+repo_name = "My First F-Droid Repo Demo"
+repo_icon = "fdroid-icon.png"
+repo_description = """
+This is a repository of apps to be used with F-Droid. Applications in this
+repository are either official binaries built by the original application
+developers, or are binaries built from source by the admin of f-droid.org
+using the tools on https://gitlab.com/u/fdroid.
+"""
+
+archive_older = 3
+archive_url = "https://f-droid.org/archive"
+archive_name = "My First F-Droid Archive Demo"
+archive_icon = "fdroid-icon.png"
+archive_description = """
+The repository of older versions of applications from the main demo repository.
+"""
+
+repo_keyalias = "sova"
+keystore = "keystore.jks"
+keystorepass = "r9aquRHYoI8+dYz6jKrLntQ5/NJNASFBacJh7Jv2BlI="
+keypass = "r9aquRHYoI8+dYz6jKrLntQ5/NJNASFBacJh7Jv2BlI="
+keydname = "CN=sova, OU=F-Droid"
+
+mirrors = (
+    'https://foo.bar/fdroid',
+    'http://foobarfoobarfoobar.onion/fdroid',
+)
+
+update_stats = True
+accepted_formats = ('json', 'txt', 'yml')
+
+install_list = 'org.adaway'
+uninstall_list = ('com.android.vending', 'com.facebook.orca', )
