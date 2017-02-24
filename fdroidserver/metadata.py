f-droid/fdroidserver

commit c0bc3afda9d67a826471df2f387ab1b489df04e0

convert metadata.Build to a subclass of dict
Like with the App class in the commit before, this makes it a lot
easier to work with this data when converting between the internal
formats and external formats like YAML, JSON, MsgPack, protobuf, etc.

The one unfortunate thing here is Build.update. It becomes
dict.update(), which is a method not an attribute.
build.get('update') or build['update'] could be used, but that would
be oddly inconsistent. So instead the field is renamed to
'androidupdate', except for in the .txt v0 metadata files. This better
describes what field does anyway, since it runs `android update`.

Build.update is only referenced in two places right next to each other
for the ant builds, so this change still seems worthwhile.

fdroidserver/metadata.py
@@ -25,7 +25,6 @@ import cgi
 import logging
 import textwrap
 import io
-import pprint
 
 import yaml
 # use libyaml if it is available
@@ -242,9 +241,10 @@ build_flags_order = [
 build_flags = set(build_flags_order + ['version', 'vercode'])
 
 
-class Build():
+class Build(dict):
 
     def __init__(self, copydict=None):
+        super().__init__()
         self.disable = False
         self.commit = None
         self.subdir = None
@@ -263,7 +263,7 @@ class Build():
         self.rm = []
         self.extlibs = []
         self.prebuild = ''
-        self.update = []
+        self.androidupdate = []
         self.target = None
         self.scanignore = []
         self.scandelete = []
@@ -274,45 +274,28 @@ class Build():
         self.gradleprops = []
         self.antcommands = []
         self.novcheck = False
-
-        self._modified = set()
-
         if copydict:
-            for k, v in copydict.items():
-                self.set_flag(k, v)
+            super().__init__(copydict)
+            return
 
-    def __str__(self):
-        return pprint.pformat(self.__dict__)
-
-    def __repr__(self):
-        return self.__str__()
-
-    def get_flag(self, f):
-        if f not in build_flags:
-            warn_or_exception('Unrecognised build flag: ' + f)
-        return getattr(self, f)
-
-    def set_flag(self, f, v):
-        if f == 'versionName':
-            f = 'version'
-        if f == 'versionCode':
-            f = 'vercode'
-        if f not in build_flags:
-            warn_or_exception('Unrecognised build flag: ' + f)
-        self.__dict__[f] = v
-        self._modified.add(f)
-
-    def append_flag(self, f, v):
-        if f not in build_flags:
-            warn_or_exception('Unrecognised build flag: ' + f)
-        if f not in self.__dict__:
-            self.__dict__[f] = [v]
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
         else:
-            self.__dict__[f].append(v)
+            raise AttributeError("No such attribute: " + name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        if name in self:
+            del self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
 
     def build_method(self):
         for f in ['maven', 'gradle', 'kivy']:
-            if self.get_flag(f):
+            if self.get(f):
                 return f
         if self.output:
             return 'raw'
@@ -323,7 +306,7 @@ class Build():
         if self.output:
             return 'raw'
         for f in ['maven', 'gradle', 'kivy']:
-            if self.get_flag(f):
+            if self.get(f):
                 return f
         return 'ant'
 
@@ -336,10 +319,6 @@ class Build():
             return ''
         return paths[version]
 
-    def update_flags(self, d):
-        for f, v in d.items():
-            self.set_flag(f, v)
-
 
 flagtypes = {
     'extlibs': TYPE_LIST,
@@ -348,7 +327,7 @@ flagtypes = {
     'rm': TYPE_LIST,
     'buildjni': TYPE_LIST,
     'preassemble': TYPE_LIST,
-    'update': TYPE_LIST,
+    'androidupdate': TYPE_LIST,
     'scanignore': TYPE_LIST,
     'scandelete': TYPE_LIST,
     'gradle': TYPE_LIST,
@@ -982,14 +961,16 @@ def parse_txt_metadata(mf, app):
 
         pk, pv = bv
         pk = pk.lstrip()
+        if pk == 'update':
+            pk = 'androidupdate'  # avoid conflicting with Build(dict).update()
         t = flagtype(pk)
         if t == TYPE_LIST:
             pv = split_list_values(pv)
-            build.set_flag(pk, pv)
+            build[pk] = pv
         elif t == TYPE_STRING or t == TYPE_SCRIPT:
-            build.set_flag(pk, pv)
+            build[pk] = pv
         elif t == TYPE_BOOL:
-            build.set_flag(pk, _decode_bool(pv))
+            build[pk] = _decode_bool(pv)
 
     def parse_buildline(lines):
         v = "".join(lines)
@@ -1231,7 +1212,7 @@ def write_plaintext_metadata(mf, app, w_comment, w_field, w_build):
         if build.version == "Ignore":
             continue
 
-        w_comments('build:' + build.vercode)
+        w_comments('build:%s' % build.vercode)
         w_build(build)
         mf.write('\n')
 
@@ -1276,11 +1257,13 @@ def write_txt(mf, app):
         mf.write("Build:%s,%s\n" % (build.version, build.vercode))
 
         for f in build_flags_order:
-            v = build.get_flag(f)
+            v = build.get(f)
             if not v:
                 continue
 
             t = flagtype(f)
+            if f == 'androidupdate':
+                f == 'update'  # avoid conflicting with Build(dict).update()
             mf.write('    %s=' % f)
             if t == TYPE_STRING:
                 mf.write(v)
@@ -1357,7 +1340,7 @@ def write_yaml(mf, app):
         w_field('versionName', build.version, '  - ', TYPE_STRING)
         w_field('versionCode', build.vercode, '    ', TYPE_STRING)
         for f in build_flags_order:
-            v = build.get_flag(f)
+            v = build.get(f)
             if not v:
                 continue
 

fdroidserver/common.py
@@ -1544,15 +1544,15 @@ def prepare_source(vcs, app, build, build_dir, srclib_dir, extlib_dir, onserver=
                                  (app.id, build.version), p.output)
 
     # Generate (or update) the ant build file, build.xml...
-    if build.build_method() == 'ant' and build.update != ['no']:
+    if build.build_method() == 'ant' and build.androidupdate != ['no']:
         parms = ['android', 'update', 'lib-project']
         lparms = ['android', 'update', 'project']
 
         if build.target:
             parms += ['-t', build.target]
             lparms += ['-t', build.target]
-        if build.update:
-            update_dirs = build.update
+        if build.androidupdate:
+            update_dirs = build.androidupdate
         else:
             update_dirs = ant_subprojects(root_dir) + ['.']
 

tests/metadata.TestCase
@@ -23,6 +23,11 @@ class MetadataTest(unittest.TestCase):
     '''fdroidserver/metadata.py'''
 
     def test_read_metadata(self):
+
+        def _build_yaml_representer(dumper, data):
+            '''Creates a YAML representation of a Build instance'''
+            return dumper.represent_dict(data)
+
         testsdir = os.path.dirname(__file__)
         os.chdir(testsdir)
 
@@ -46,6 +51,7 @@ class MetadataTest(unittest.TestCase):
             self.assertEqual(frommeta, frompickle)
             # Uncomment to overwrite
             # with open(savepath, 'w') as f:
+            #     yaml.add_representer(fdroidserver.metadata.Build, _build_yaml_representer)
             #     yaml.dump(frommeta, f, default_flow_style=False)
 
 

tests/metadata/dump/org.adaway.yaml
@@ -58,10 +58,10 @@ VercodeOperation: null
 WebSite: http://sufficientlysecure.org/index.php/adaway
 added: null
 builds:
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: ea5378a94ee0dc1d99d2cec95fae7e6d81afb2b9
   disable: false
   encoding: null
@@ -87,13 +87,12 @@ builds:
   subdir: org_adaway/
   submodules: false
   target: null
-  update: []
   vercode: '13'
   version: '1.12'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: 4128e59da2eac5c2904c7c7568d298ca51e79540
   disable: false
   encoding: null
@@ -120,13 +119,12 @@ builds:
   subdir: org_adaway/
   submodules: false
   target: null
-  update: []
   vercode: '16'
   version: '1.15'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: 0b9985398b9eef7baf6aadd0dbb12002bc199d2e
   disable: false
   encoding: null
@@ -153,13 +151,12 @@ builds:
   subdir: org_adaway/
   submodules: false
   target: null
-  update: []
   vercode: '19'
   version: '1.18'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: ab27f4dab5f3ea5e228cfb4a6b0e1fbf53695f22
   disable: false
   encoding: null
@@ -186,13 +183,12 @@ builds:
   subdir: org_adaway/
   submodules: false
   target: null
-  update: []
   vercode: '20'
   version: '1.19'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: 695e3801e4081026c8f7213a2345fc451d5eb89c
   disable: false
   encoding: null
@@ -219,13 +215,12 @@ builds:
   subdir: org_adaway/
   submodules: false
   target: null
-  update: []
   vercode: '21'
   version: '1.20'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: 65138c11cc8b6affd28b68e125fbc1dff0886a4e
   disable: false
   encoding: null
@@ -252,10 +247,10 @@ builds:
   subdir: org_adaway/
   submodules: false
   target: null
-  update: []
   vercode: '22'
   version: '1.21'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
   buildjni: []
   commit: unknown - see disabled
@@ -283,13 +278,12 @@ builds:
   subdir: null
   submodules: false
   target: null
-  update: []
   vercode: '24'
   version: '1.23'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: f811e53e1e1d2ee047b18715fd7d2072b90ae76b
   disable: false
   encoding: null
@@ -315,13 +309,12 @@ builds:
   subdir: org_adaway/
   submodules: false
   target: null
-  update: []
   vercode: '25'
   version: '1.24'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: ff97932761cdee68638dc2550751a64b2cbe18e7
   disable: false
   encoding: null
@@ -347,13 +340,12 @@ builds:
   subdir: org_adaway/
   submodules: false
   target: null
-  update: []
   vercode: '26'
   version: '1.25'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: 33d4d80998f30bafc88c04c80cbae00b03916f99
   disable: false
   encoding: null
@@ -379,13 +371,12 @@ builds:
   subdir: org_adaway/
   submodules: false
   target: null
-  update: []
   vercode: '27'
   version: '1.26'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: 743d25a7e287505461f33f4b8e57e4cf988fffea
   disable: false
   encoding: null
@@ -411,13 +402,12 @@ builds:
   subdir: org_adaway/
   submodules: false
   target: null
-  update: []
   vercode: '28'
   version: '1.27'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: eaa07f4
   disable: false
   encoding: null
@@ -443,10 +433,10 @@ builds:
   subdir: org_adaway/
   submodules: false
   target: null
-  update: []
   vercode: '30'
   version: '1.29'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
   buildjni: false
   commit: 71ced3f
@@ -475,10 +465,10 @@ builds:
   subdir: org_adaway/
   submodules: false
   target: null
-  update: []
   vercode: '33'
   version: '1.32'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
   buildjni: false
   commit: 9d63c18
@@ -506,10 +496,10 @@ builds:
   subdir: org_adaway/
   submodules: false
   target: null
-  update: []
   vercode: '34'
   version: '1.33'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
   buildjni: false
   commit: f2568b1
@@ -538,10 +528,10 @@ builds:
   subdir: org_adaway/
   submodules: false
   target: null
-  update: []
   vercode: '35'
   version: '1.34'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
   buildjni: false
   commit: 7442d5d
@@ -570,10 +560,10 @@ builds:
   subdir: org_adaway/
   submodules: false
   target: null
-  update: []
   vercode: '36'
   version: '1.35'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
   buildjni: false
   commit: 83fc713
@@ -602,10 +592,10 @@ builds:
   subdir: org_adaway/
   submodules: false
   target: null
-  update: []
   vercode: '37'
   version: '1.36'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
   buildjni: false
   commit: 70da32b567122b701cdcb1609b780eb85732028f
@@ -634,13 +624,16 @@ builds:
   subdir: org_adaway/
   submodules: false
   target: null
-  update: []
   vercode: '38'
   version: '1.37'
-- antcommands: []
+- androidupdate:
+  - .
+  - android-libs/Donations
+  - android-libs/ActionBarSherlock
+  - android-libs/HtmlSpanner/htmlspanner
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: v2.1
   disable: false
   encoding: null
@@ -674,17 +667,16 @@ builds:
   subdir: AdAway
   submodules: false
   target: null
-  update:
+  vercode: '40'
+  version: '2.1'
+- androidupdate:
   - .
   - android-libs/Donations
   - android-libs/ActionBarSherlock
   - android-libs/HtmlSpanner/htmlspanner
-  vercode: '40'
-  version: '2.1'
-- antcommands: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: v2.3
   disable: false
   encoding: null
@@ -718,25 +710,19 @@ builds:
   subdir: AdAway
   submodules: false
   target: null
-  update:
-  - .
-  - android-libs/Donations
-  - android-libs/ActionBarSherlock
-  - android-libs/HtmlSpanner/htmlspanner
   vercode: '42'
   version: '2.3'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: v2.6
   disable: false
   encoding: null
   extlibs: []
   forcevercode: false
   forceversion: false
-  gradle:
-  - 'yes'
+  gradle: true
   gradleprops: []
   init: ''
   kivy: false
@@ -756,21 +742,19 @@ builds:
   subdir: AdAway
   submodules: false
   target: null
-  update: []
   vercode: '45'
   version: '2.6'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: v2.7
   disable: false
   encoding: null
   extlibs: []
   forcevercode: false
   forceversion: false
-  gradle:
-  - 'yes'
+  gradle: true
   gradleprops: []
   init: ''
   kivy: false
@@ -790,21 +774,19 @@ builds:
   subdir: AdAway
   submodules: false
   target: null
-  update: []
   vercode: '46'
   version: '2.7'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: v2.8
   disable: false
   encoding: null
   extlibs: []
   forcevercode: false
   forceversion: false
-  gradle:
-  - 'yes'
+  gradle: true
   gradleprops: []
   init: ''
   kivy: false
@@ -824,21 +806,19 @@ builds:
   subdir: AdAway
   submodules: false
   target: null
-  update: []
   vercode: '47'
   version: '2.8'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: v2.8.1
   disable: false
   encoding: null
   extlibs: []
   forcevercode: false
   forceversion: false
-  gradle:
-  - 'yes'
+  gradle: true
   gradleprops: []
   init: ''
   kivy: false
@@ -858,21 +838,19 @@ builds:
   subdir: AdAway
   submodules: false
   target: null
-  update: []
   vercode: '48'
   version: 2.8.1
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: v2.9
   disable: false
   encoding: null
   extlibs: []
   forcevercode: false
   forceversion: false
-  gradle:
-  - 'yes'
+  gradle: true
   gradleprops: []
   init: ''
   kivy: false
@@ -892,21 +870,19 @@ builds:
   subdir: AdAway
   submodules: false
   target: null
-  update: []
   vercode: '49'
   version: '2.9'
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: v2.9.1
   disable: false
   encoding: null
   extlibs: []
   forcevercode: false
   forceversion: false
-  gradle:
-  - 'yes'
+  gradle: true
   gradleprops: []
   init: ''
   kivy: false
@@ -926,21 +902,19 @@ builds:
   subdir: AdAway
   submodules: false
   target: null
-  update: []
   vercode: '50'
   version: 2.9.1
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: v2.9.2
   disable: false
   encoding: null
   extlibs: []
   forcevercode: false
   forceversion: false
-  gradle:
-  - 'yes'
+  gradle: true
   gradleprops: []
   init: ''
   kivy: false
@@ -960,21 +934,19 @@ builds:
   subdir: AdAway
   submodules: false
   target: null
-  update: []
   vercode: '51'
   version: 2.9.2
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
-  buildjni:
-  - 'yes'
+  buildjni: true
   commit: v3.0
   disable: false
   encoding: null
   extlibs: []
   forcevercode: false
   forceversion: false
-  gradle:
-  - 'yes'
+  gradle: true
   gradleprops: []
   init: ''
   kivy: false
@@ -994,7 +966,6 @@ builds:
   subdir: AdAway
   submodules: false
   target: null
-  update: []
   vercode: '52'
   version: '3.0'
 comments:

tests/metadata/dump/org.smssecure.smssecure.yaml
@@ -55,7 +55,8 @@ VercodeOperation: null
 WebSite: http://www.smssecure.org
 added: null
 builds:
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
   buildjni: []
   commit: 66367479a4f57f347b5cbe8f6f8f632adaae7727
@@ -95,10 +96,10 @@ builds:
   subdir: null
   submodules: false
   target: null
-  update: []
   vercode: '5'
   version: 0.3.3
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
   buildjni: []
   commit: 9675ce5eecb929dcaddb43b3d9486fdb88b9ae1a
@@ -130,10 +131,10 @@ builds:
   subdir: null
   submodules: true
   target: null
-  update: []
   vercode: '6'
   version: 0.3.3
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
   buildjni: []
   commit: v0.4.2
@@ -164,10 +165,10 @@ builds:
   subdir: null
   submodules: true
   target: null
-  update: []
   vercode: '9'
   version: 0.4.2
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
   buildjni: []
   commit: v0.5.1
@@ -198,10 +199,10 @@ builds:
   subdir: null
   submodules: true
   target: null
-  update: []
   vercode: '11'
   version: 0.5.1
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
   buildjni: []
   commit: v0.5.2
@@ -231,10 +232,10 @@ builds:
   subdir: null
   submodules: true
   target: null
-  update: []
   vercode: '12'
   version: 0.5.2
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
   buildjni: []
   commit: v0.5.3
@@ -264,10 +265,10 @@ builds:
   subdir: null
   submodules: true
   target: null
-  update: []
   vercode: '100'
   version: 0.5.3
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
   buildjni: []
   commit: v0.5.4
@@ -297,10 +298,10 @@ builds:
   subdir: null
   submodules: true
   target: null
-  update: []
   vercode: '101'
   version: 0.5.4
-- antcommands: []
+- androidupdate: []
+  antcommands: []
   build: ''
   buildjni: []
   commit: v0.6.0
@@ -330,7 +331,6 @@ builds:
   subdir: null
   submodules: true
   target: null
-  update: []
   vercode: '102'
   version: 0.6.0
 comments: {}

commit 7a25dbc081d7ec677760b5bd7efc658def6b0fd3

rewritemeta: fix silly bug of using == for =
This was introduced in c0bc3afda9d67a826471df2f387ab1b489df04e0

fdroidserver/metadata.py
@@ -224,7 +224,7 @@ build_flags_order = [
     'rm',
     'extlibs',
     'prebuild',
-    'update',
+    'androidupdate',
     'target',
     'scanignore',
     'scandelete',
@@ -1275,7 +1275,7 @@ def write_txt(mf, app):
 
             t = flagtype(f)
             if f == 'androidupdate':
-                f == 'update'  # avoid conflicting with Build(dict).update()
+                f = 'update'  # avoid conflicting with Build(dict).update()
             mf.write('    %s=' % f)
             if t == TYPE_STRING:
                 mf.write(v)