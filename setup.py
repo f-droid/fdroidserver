6d69dcddd95fe08ffe431e305932cfdeafd6fc9d
Hans-Christoph Steiner <hans@eds.org>
2018-01-03 21:41:35 +0100

    use 1.0.0 for release version to make pypi happy

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ else:
     readme = ''
 
 setup(name='fdroidserver',
-      version='1.0',
+      version='1.0.0',
       description='F-Droid Server Tools',
       long_description=readme,
       author='The F-Droid Project',
