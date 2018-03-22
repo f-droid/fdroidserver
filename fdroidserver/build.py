4ca77a4fafc9257d123c1ef558116d8255a14e11
Hans-Christoph Steiner <hans@eds.org>
2018-03-22 17:16:09 +0100

    update: strip all whitespace from buildserverid

--- fdroidserver/build.py
+++ fdroidserver/build.py
@@ -79,7 +79,7 @@ def build_server(app, build, vcs, build_dir, output_dir, log_dir, force):
         if not buildserverid:
             buildserverid = subprocess.check_output(['vagrant', 'ssh', '-c',
                                                      'cat /home/vagrant/buildserverid'],
-                                                    cwd='builder').rstrip().decode()
+                                                    cwd='builder').strip().decode()
             logging.debug(_('Fetched buildserverid from VM: {buildserverid}')
                           .format(buildserverid=buildserverid))
 
