3cb4aa6c18037a56d8f95c07abb1d05d4c20fba5
Hans-Christoph Steiner <hans@eds.org>
2018-05-15 12:38:51 +0200

    bump to 1.0.4

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ else:
     readme = ''
 
 setup(name='fdroidserver',
-      version='1.0.3',
+      version='1.0.4',
       description='F-Droid Server Tools',
       long_description=readme,
       author='The F-Droid Project',
