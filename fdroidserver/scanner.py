f-droid/fdroidserver

commit 44e8f425d5173d9177ed67496e1bbb374bfd24f4

fix PEP8 formatting issue
fdroidserver/scanner.py:35:34: E241 multiple spaces after ','
fdroidserver/scanner.py:36:35: E241 multiple spaces after ','
fdroidserver/scanner.py:37:30: E241 multiple spaces after ','
fdroidserver/scanner.py:38:41: E241 multiple spaces after ','
fdroidserver/scanner.py:39:30: E241 multiple spaces after ','
fdroidserver/scanner.py:40:38: E241 multiple spaces after ','
fdroidserver/scanner.py:41:38: E241 multiple spaces after ','

fdroidserver/scanner.py
@@ -32,13 +32,20 @@ options = None
 
 
 def get_gradle_compile_commands(build):
-    compileCommands = ['compile',          'releaseCompile'
-                       'provided',         'releaseProvided',
-                       'apk',              'releaseApk',
-                       'implementation',   'releaseImplementation',
-                       'api',              'releaseApi',
-                       'compileOnly',      'releaseCompileOnly',
-                       'runtimeOnly',      'releaseRuntimeOnly']
+    compileCommands = ['compile',
+                       'provided'
+                       'apk'
+                       'implementation'
+                       'api'
+                       'compileOnly'
+                       'runtimeOnly',
+                       'releaseCompile'
+                       'releaseProvided'
+                       'releaseApk'
+                       'releaseImplementation'
+                       'releaseApi'
+                       'releaseCompileOnly'
+                       'releaseRuntimeOnly']
     if build.gradle and build.gradle != ['yes']:
         compileCommands += [flavor + 'Compile' for flavor in build.gradle]
         compileCommands += [flavor + 'ReleaseCompile' for flavor in build.gradle]

commit c44ab190bd28e9cbed67423072bcc2a22cbdf993

scanner: fix reformatting fail
This was broken by 44e8f425d :-(.

fdroidserver/scanner.py
@@ -33,18 +33,18 @@ options = None
 
 def get_gradle_compile_commands(build):
     compileCommands = ['compile',
-                       'provided'
-                       'apk'
-                       'implementation'
-                       'api'
-                       'compileOnly'
+                       'provided',
+                       'apk',
+                       'implementation',
+                       'api',
+                       'compileOnly',
                        'runtimeOnly',
-                       'releaseCompile'
-                       'releaseProvided'
-                       'releaseApk'
-                       'releaseImplementation'
-                       'releaseApi'
-                       'releaseCompileOnly'
+                       'releaseCompile',
+                       'releaseProvided',
+                       'releaseApk',
+                       'releaseImplementation',
+                       'releaseApi',
+                       'releaseCompileOnly',
                        'releaseRuntimeOnly']
     if build.gradle and build.gradle != ['yes']:
         compileCommands += [flavor + 'Compile' for flavor in build.gradle]