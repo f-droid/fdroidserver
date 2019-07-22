7c4bed05580171e4e86a5ac0ad977ae11899c4aa
Gerhard Olsson <gerhardol@users.noreply.github.com>
2019-07-23 00:21:28 +0200

    UpdateCheckIgnore: Exception if version not matched
    Ignoreversions were checked also if version did not match which raised the exception
    The 'version check' runs in some unexpected situations like just
    to find directories where to find the AutoName.

--- fdroidserver/checkupdates.py
+++ fdroidserver/checkupdates.py
@@ -43,6 +43,9 @@ from .exception import VCSException, NoSubmodulesException, FDroidException, Met
 # required.
 def check_http(app):
 
+    ignoreversions = app.UpdateCheckIgnore
+    ignoresearch = re.compile(ignoreversions).search if ignoreversions else None
+
     try:
 
         if not app.UpdateCheckData:
@@ -82,8 +85,9 @@ def check_http(app):
                 raise FDroidException("No RE match for version")
             version = m.group(1)
 
-        return (version, vercode)
-
+        if version and ignoresearch and not ignoresearch(version):
+            return (version, vercode)
+        return (None, ("Version {version} is ignored").format(version=version))
     except FDroidException:
         msg = "Could not complete http check for app {0} due to unknown error: {1}".format(app.id, traceback.format_exc())
         return (None, msg)
