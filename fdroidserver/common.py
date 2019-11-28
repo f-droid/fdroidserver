0e071a689d5aa7dcbbe22537087b0eb69bf2b76f
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2019-11-28 21:44:28 +0100

    Support hex versionCode in build command line
    Example: fdroid build net.gaast.giggity:0x200

--- fdroidserver/common.py
+++ fdroidserver/common.py
@@ -497,6 +497,11 @@ def read_pkg_args(appid_versionCode_pairs, allow_vercodes=False):
     for p in appid_versionCode_pairs:
         if allow_vercodes and ':' in p:
             package, vercode = p.split(':')
+            try:
+                i_vercode = int(vercode, 0)
+            except ValueError:
+                i_vercode = int(vercode)
+            vercode = str(i_vercode)
         else:
             package, vercode = p, None
         if package not in vercodes:
