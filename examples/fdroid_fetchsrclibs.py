330ca2319ce65a1de5ed3f73a8d7003fc4535408
Hans-Christoph Steiner <hans@eds.org>
2020-06-23 21:56:15 +0200

    example plugin for fetching srclibs
    This is used in _fdroiddata_'s `fdroid build` CI job, and _issuebot_.
    This uses @uniqx's awesome new 'plugin' feature to create a 'fetchsrclibs`
    command.  The 'fdroid build' gitlab-ci job uses --on-server, which does not
    set up the srclibs.  This plugin does the missing setup.
    
    This is moved from _fdroiddata_ where it was _tools/fdroid_fetchsrclibs.py_.

---
+++ examples/fdroid_fetchsrclibs.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+#
+# an fdroid plugin for setting up srclibs
+#
+# The 'fdroid build' gitlab-ci job uses --on-server, which does not
+# set up the srclibs.  This plugin does the missing setup.
+
+import argparse
+import os
+import pprint
+from fdroidserver import _, common, metadata
+
+fdroid_summary = 'prepare the srclibs for `fdroid build --on-server`'
+
+
+def main():
+    common.config = {
+        'accepted_formats': 'yml',
+        'sdk_path': os.getenv('ANDROID_HOME'),
+    }
+    common.fill_config_defaults(common.config)
+    parser = argparse.ArgumentParser(usage="%(prog)s [options] [APPID[:VERCODE] [APPID[:VERCODE] ...]]")
+    common.setup_global_opts(parser)
+    parser.add_argument("appid", nargs='*', help=_("applicationId with optional versionCode in the form APPID[:VERCODE]"))
+    metadata.add_metadata_arguments(parser)
+    options = parser.parse_args()
+    common.options = options
+    pkgs = common.read_pkg_args(options.appid, True)
+    allapps = metadata.read_metadata(pkgs)
+    apps = common.read_app_args(options.appid, allapps, True)
+    srclib_dir = os.path.join('build', 'srclib')
+    os.makedirs(srclib_dir, exist_ok=True)
+    srclibpaths = []
+    for appid, app in apps.items():
+        for build in app.get('Builds', []):
+            for lib in build.srclibs:
+                srclibpaths.append(common.getsrclib(lib, srclib_dir, build=build))
+    print('Set up srclibs:')
+    pprint.pprint(srclibpaths)
+
+
+if __name__ == "__main__":
+    main()
