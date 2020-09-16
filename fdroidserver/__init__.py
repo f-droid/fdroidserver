75c4be2ea953192b06121b72cabe16936f286127
Michael Pöhn <michael@poehn.at>
2020-09-16 18:26:16 +0200

    expose public api in fdroidserver module

--- fdroidserver/__init__.py
+++ fdroidserver/__init__.py
@@ -21,3 +21,36 @@ for rootpath in rootpaths:
 gettext.bindtextdomain('fdroidserver', localedir)
 gettext.textdomain('fdroidserver')
 _ = gettext.gettext
+
+
+from fdroidserver.exception import (FDroidException,
+                                    MetaDataException,
+                                    VerificationException)  # NOQA: E402
+FDroidException  # NOQA: B101
+MetaDataException  # NOQA: B101
+VerificationException  # NOQA: B101
+
+from fdroidserver.common import (verify_apk_signature,
+                                 genkeystore as generate_keystore)  # NOQA: E402
+verify_apk_signature  # NOQA: B101
+generate_keystore  # NOQA: B101
+from fdroidserver.index import (download_repo_index,
+                                get_mirror_service_urls,
+                                make as make_index)  # NOQA: E402
+download_repo_index  # NOQA: B101
+get_mirror_service_urls  # NOQA: B101
+make_index  # NOQA: B101
+from fdroidserver.update import (process_apk,
+                                 process_apks,
+                                 scan_apk,
+                                 scan_repo_files)  # NOQA: E402
+process_apk  # NOQA: B101
+process_apks  # NOQA: B101
+scan_apk  # NOQA: B101
+scan_repo_files  # NOQA: B101
+from fdroidserver.server import (update_awsbucket,
+                                 update_servergitmirrors,
+                                 update_serverwebroot)  # NOQA: E402
+update_awsbucket  # NOQA: B101
+update_servergitmirrors  # NOQA: B101
+update_serverwebroot  # NOQA: B101
