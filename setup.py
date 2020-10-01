9c852d4e8d06e2afb24f48af7323bd50e972b55a
Hans-Christoph Steiner <hans@eds.org>
2020-10-01 14:55:32 +0200

    first alpha 2.0 release 2.0a0

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='fdroidserver',
-      version='1.2a',
+      version='2.0a0',
       description='F-Droid Server Tools',
       long_description=long_description,
       long_description_content_type='text/markdown',
