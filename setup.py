e8395b5fe93def9358dfc71ba76d890548ad83f6
Hans-Christoph Steiner <hans@eds.org>
2020-11-27 11:00:08 +0100

    release v2.0a3
    somehow I messed up and uploaded 2.0a2 that was incomplete, so now 2.0a3

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='fdroidserver',
-      version='2.0a2',
+      version='2.0a3',
       description='F-Droid Server Tools',
       long_description=long_description,
       long_description_content_type='text/markdown',
