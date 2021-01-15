d16ccc6d6ce52e693eeef5573663793728fa81c7
Hans-Christoph Steiner <hans@eds.org>
2021-01-15 15:11:50 +0100

    version 2.0b

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='fdroidserver',
-      version='2.0a5',
+      version='2.0b',
       description='F-Droid Server Tools',
       long_description=long_description,
       long_description_content_type='text/markdown',
