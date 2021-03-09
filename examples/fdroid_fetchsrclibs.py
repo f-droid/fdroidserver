f-droid/fdroidserver

commit 54e8867a7ed2918709290be8f8fc1e8b681767e4

fix read_metadata in `fdroid build`
Broken in fdroid/fdroidserver!828

fdroidserver/build.py
@@ -1005,7 +1005,7 @@ def main():
 
     # Read all app and srclib metadata
     pkgs = common.read_pkg_args(options.appid, True)
-    allapps = metadata.read_metadata(not options.onserver, pkgs, options.refresh, sort_by_time=True)
+    allapps = metadata.read_metadata(pkgs, options.refresh, sort_by_time=True)
     apps = common.read_app_args(options.appid, allapps, True)
 
     for appid, app in list(apps.items()):

fdroidserver/metadata.py
@@ -541,7 +541,7 @@ def read_srclibs():
         srclibs[srclibname] = parse_yaml_srclib(metadatapath)
 
 
-def read_metadata(appids=None, check_vcs=[], refresh=True, sort_by_time=False):
+def read_metadata(appids={}, refresh=True, sort_by_time=False):
     """Return a list of App instances sorted newest first
 
     This reads all of the metadata files in a 'data' repository, then
@@ -549,7 +549,7 @@ def read_metadata(appids=None, check_vcs=[], refresh=True, sort_by_time=False):
     sorted based on creation time, newest first.  Most of the time,
     the newer files are the most interesting.
 
-    check_vcs is the list of appids to check for .fdroid.yml in source
+    appids is a dict with appids a keys and versionCodes as values.
 
     """
 
@@ -597,7 +597,7 @@ def read_metadata(appids=None, check_vcs=[], refresh=True, sort_by_time=False):
         if appid in apps:
             _warn_or_exception(_("Found multiple metadata files for {appid}")
                                .format(appid=appid))
-        app = parse_metadata(metadatapath, appid in check_vcs, refresh)
+        app = parse_metadata(metadatapath, appid in appids, refresh)
         check_metadata(app)
         apps[app.id] = app
 

commit 92438bbf78532b89fb7619601cf75c95e7d7f0a0

metadata: stop setting up source repo when running lint/rewritemeta
broken in 54e8867a7ed2918709290be8f8fc1e8b681767e4

examples/fdroid_fetchsrclibs.py
@@ -21,7 +21,7 @@ def main():
     options = parser.parse_args()
     common.options = options
     pkgs = common.read_pkg_args(options.appid, True)
-    allapps = metadata.read_metadata(pkgs)
+    allapps = metadata.read_metadata(pkgs, check_vcs=True)
     apps = common.read_app_args(options.appid, allapps, True)
     srclib_dir = os.path.join('build', 'srclib')
     os.makedirs(srclib_dir, exist_ok=True)

fdroidserver/build.py
@@ -1013,7 +1013,7 @@ def main():
 
     # Read all app and srclib metadata
     pkgs = common.read_pkg_args(options.appid, True)
-    allapps = metadata.read_metadata(pkgs, options.refresh, sort_by_time=True)
+    allapps = metadata.read_metadata(pkgs, options.refresh, sort_by_time=True, check_vcs=True)
     apps = common.read_app_args(options.appid, allapps, True)
 
     for appid, app in list(apps.items()):

fdroidserver/metadata.py
@@ -541,7 +541,7 @@ def read_srclibs():
         srclibs[srclibname] = parse_yaml_srclib(metadatapath)
 
 
-def read_metadata(appids={}, refresh=True, sort_by_time=False):
+def read_metadata(appids={}, refresh=True, sort_by_time=False, check_vcs=False):
     """Return a list of App instances sorted newest first
 
     This reads all of the metadata files in a 'data' repository, then
@@ -597,7 +597,7 @@ def read_metadata(appids={}, refresh=True, sort_by_time=False):
         if appid in apps:
             _warn_or_exception(_("Found multiple metadata files for {appid}")
                                .format(appid=appid))
-        app = parse_metadata(metadatapath, appid in appids, refresh)
+        app = parse_metadata(metadatapath, check_vcs, refresh)
         check_metadata(app)
         apps[app.id] = app
 