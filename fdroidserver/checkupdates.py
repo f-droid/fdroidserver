f-droid/fdroidserver

commit cd405cc93e904f91b614313791a0d9802cbe8d3c

Use hash of tag when adding new version
Also extend getref with a refname parameter and implement it for
vcs_git.

fdroidserver/checkupdates.py
@@ -217,6 +217,12 @@ def check_tags(app, pattern):
                             hver = version
 
         if hver:
+            try:
+                commit = vcs.getref(htag)
+                if commit:
+                    return (hver, hcode, commit)
+            except VCSException:
+                pass
             return (hver, hcode, htag)
         return (None, "Couldn't find any version information", None)
 

fdroidserver/common.py
@@ -1026,7 +1026,7 @@ class vcs:
         """Get a list of all the known tags, sorted from newest to oldest"""
         raise VCSException('latesttags not supported for this vcs type')
 
-    def getref(self):
+    def getref(self, revname=None):
         """Get current commit reference (hash, revision, etc)"""
         raise VCSException('getref not supported for this vcs type')
 
@@ -1205,6 +1205,15 @@ class vcs_git(vcs):
                     tags.append(tag)
         return tags
 
+    def getref(self, revname='HEAD'):
+        self.checkrepo()
+        p = FDroidPopen(['git', 'rev-parse', '--verify',
+                         '{revname}^{{commit}}'.format(revname=revname)], cwd=self.local,
+                        output=False)
+        if p.returncode != 0:
+            return None
+        return p.output.strip()
+
 
 class vcs_gitsvn(vcs):
 
@@ -1366,9 +1375,9 @@ class vcs_gitsvn(vcs):
             if os.path.isdir(d):
                 return os.listdir(d)
 
-    def getref(self):
+    def getref(self, revname='HEAD'):
         self.checkrepo()
-        p = FDroidPopen(['git', 'svn', 'find-rev', 'HEAD'], cwd=self.local, output=False)
+        p = FDroidPopen(['git', 'svn', 'find-rev', revname], cwd=self.local, output=False)
         if p.returncode != 0:
             return None
         return p.output.strip()

commit b49c3741bde2a3d2036837ee737446d80c7a4d18

[checkupdates] Use tag if version is 'Unknown'
Regression from cd405cc9.
Parse_androidmanifests() can return 'Unknown' or 'Ignore' if it did not
find a version name. The check_tags() always returned the tag and
checkupdates_app() replaced the version by the tag in the 'Unknown'
case. Since cd405cc9 the tag is the hash and so the version would become
the hash as well. This patch moves the 'Unknown' check directly after
the Parse_androidmanifests().

fdroidserver/checkupdates.py
@@ -207,6 +207,8 @@ def check_tags(app, pattern):
                     root_dir = build_dir / subdir
                     paths = common.manifest_paths(root_dir, last_build.gradle)
                     version, vercode, _package = common.parse_androidmanifests(paths, app)
+                    if version == 'Unknown' or version == 'Ignore':
+                        version = tag
                     if vercode:
                         logging.debug("Manifest exists in subdir '{0}'. Found version {1} ({2})"
                                       .format(subdir, version, vercode))
@@ -463,8 +465,6 @@ def checkupdates_app(app):
     if mode.startswith('Tags'):
         pattern = mode[5:] if len(mode) > 4 else None
         (version, vercode, tag) = check_tags(app, pattern)
-        if version == 'Unknown':
-            version = tag
         msg = vercode
     elif mode == 'RepoManifest':
         (version, vercode) = check_repomanifest(app)