beb49eea48b97ee734d49112e9bbccd8e0a8f7cd
FestplattenSchnitzel <festplatte.schnitzel@posteo.de>
2022-07-31 21:52:54 +0200

    Stop packaging makebuildserver script
    makebuildserver requires this repository to be cloned locally.
    Fixes #1014.

--- setup.py
+++ setup.py
@@ -80,7 +80,6 @@ setup(
     url='https://f-droid.org',
     license='AGPL-3.0',
     packages=['fdroidserver', 'fdroidserver.asynchronousfilereader'],
-    scripts=['makebuildserver'],
     entry_points={'console_scripts': ['fdroid=fdroidserver.__main__:main']},
     data_files=get_data_files(),
     python_requires='>=3.9',
