b45b31b927b559fd279dced73b207947f10e6627
Hans-Christoph Steiner <hans@eds.org>
2024-05-13 10:36:56 +0200

    version 2.3 alpha 1

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='fdroidserver',
-    version='2.3a0',
+    version='2.3a1',
     description='F-Droid Server Tools',
     long_description=long_description,
     long_description_content_type='text/markdown',
