f-droid/fdroidserver

commit 548f73d98877c3d69323daa9d3e3b8d5defc68bf

nightly: search subdirs called 'fdroid' for APKs
@MTRNord tracked down this bug while working on his app
https://gitlab.com/Nordgedanken/simplematrix

fdroidserver/nightly.py
@@ -249,8 +249,8 @@ Last updated: {date}'''.format(repo_git_base=repo_git_base,
         common.assert_config_keystore(config)
 
         for root, dirs, files in os.walk(cibase):
-            for d in ('fdroid', '.git', '.gradle'):
-                if d in dirs:
+            for d in dirs:
+                if d == '.git' or d == '.gradle' or (d == 'fdroid' and root == cibase):
                     dirs.remove(d)
             for f in files:
                 if f.endswith('-debug.apk'):

commit 8e89cf511d8d1ecc18476df98b0b29d6a771480e

Correct traversing cibase
Partly revert d9aab228 and fix bug introduced in 548f73d9:
modifying list that is being iterated over

fdroidserver/nightly.py
@@ -438,17 +438,12 @@ Last updated: {date}'''.format(repo_git_base=repo_git_base,
         )
 
         for root, dirs, files in os.walk(cibase):
-            if root.startswith(repo_basedir):
-                logging.error(
-                    _('Broken dirs.remove(d): {root} is part of {repo_basedir} and should not be scanned').format(
-                        root=root,
-                        repo_basedir=repo_basedir
-                    )
-                )
-                continue
-            for d in dirs:
-                if d == '.git' or d == '.gradle' or (d == 'fdroid' and root == cibase):
+            for d in ('.git', '.gradle'):
+                if d in dirs:
                     dirs.remove(d)
+            if root == cibase and 'fdroid' in dirs:
+                dirs.remove('fdroid')
+
             for f in files:
                 if f.endswith('-debug.apk'):
                     apkfilename = os.path.join(root, f)